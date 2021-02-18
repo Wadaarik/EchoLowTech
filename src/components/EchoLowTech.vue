744efb8c400538c92baaf908ea9ef40bd5048b8f
Wadaarik <nicolasboudier12@gmail.com>
2021-02-18 15:07:09 +0100

    fourth commit

--- src/components/EchoLowTech.vue
+++ src/components/EchoLowTech.vue
@@ -7,7 +7,7 @@
     <div id="main-echolowtech">
       <section>
         <p>
-          <span>Chez EchoLowTech, une petite, mais dynamique association spécialisée dans l'adoption des low-tech dans les entreprises, nous sommes animés par la passion du respect de l'environnement et de l’efficacité du service client. C’est pourquoi nous nous engageons à vous apporter les deux..</span>
+          <span>Chez EchoLowTech, une petite, mais dynamique association spécialisée dans l'adoption des low-tech dans les entreprises, nous sommes animés par la passion du respect de l'environnement et de l’efficacité du service client.<br/>C’est pourquoi nous nous engageons à vous apporter les deux..</span>
           <span>Si vous cherchez quelque chose de nouveau, vous êtes au bon endroit. Notre mission est de tout faire pour répondre à votre de demande de conversion aux low-tech.</span>
         </p>
       </section>
